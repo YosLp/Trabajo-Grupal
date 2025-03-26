4cffb8d1cb53cb9ee3146a210d1f9723c0fbecc1
Santiard <jesardila17@gmail.com>
2025-03-26 10:20:27 -0500

    First Commit
    First commit with the class user with all the folders needed

---
+++ demo/src/main/java/co/edu/udes/activity/backend/demo/models/User.java
@@ -0,0 +1,37 @@
+package co.edu.udes.activity.backend.demo.models;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name= "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    public User (){
+
+
+    }
+
+}
+
+
+
